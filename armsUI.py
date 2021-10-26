be5daf77773256d0eb1ffc85e1eb6d5159203f2e
266-750Balloons <79549730+266-750Balloons@users.noreply.github.com>
2021-10-26 15:00:06 -0700

    add character functions

--- armsUI.py
+++ armsUI.py
@@ -50,3 +50,110 @@ def purple():
 def other():
     message.append("Other")
 
+def letter_A() :
+     message.append("A")
+
+def letter_B() :
+     message.append("B")
+
+def letter_C() :
+     message.append("C")
+
+def letter_D() :
+     message.append("D")
+
+def letter_E() :
+     message.append("E")
+
+def letter_F() :
+     message.append("F")
+
+def letter_G() :
+     message.append("G")
+
+def letter_H() :
+     message.append("H")
+
+def letter_I() :
+     message.append("I")
+
+def letter_J() :
+     message.append("J")
+
+def letter_K() :
+     message.append("K")
+
+def letter_L() :
+     message.append("L")
+
+def letter_M() :
+     message.append("M")
+
+def letter_N() :
+     message.append("N")
+
+def letter_O() :
+     message.append("O")
+
+def letter_P() :
+     message.append("P")
+
+def letter_Q() :
+     message.append("Q")
+
+def letter_R() :
+     message.append("R")
+
+def letter_S() :
+     message.append("S")
+
+def letter_T() :
+     message.append("T")
+
+def letter_U() :
+     message.append("U")
+
+def letter_V() :
+     message.append("V")
+
+def letter_W() :
+     message.append("W")
+
+def letter_X() :
+     message.append("X")
+
+def letter_Y() :
+     message.append("Y")
+
+def letter_Z() :
+     message.append("Z")
+
+def letter_1() :
+     message.append("1")
+
+def letter_2() :
+     message.append("2")
+
+def letter_3() :
+     message.append("3")
+
+def letter_4() :
+     message.append("4")
+
+def letter_5() :
+     message.append("5")
+
+def letter_6() :
+     message.append("6")
+
+def letter_7() :
+     message.append("7")
+
+def letter_8() :
+     message.append("8")
+
+def letter_9() :
+     message.append("9")
+
+def letter_0() :
+     message.append("0")
